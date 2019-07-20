92eee069553e07ad01f6502365d5b0ebf0ccbb5f
Star <jessty@foxmail.com>
2019-07-20 17:53:42 +0800

    rewrite pixi-hammer feat:1.Classify 2.Add simple bubble phase 3.SubClass of Hammer.manager

--- index.js
+++ index.js
@@ -1,122 +1,165 @@
-var Hammer = require('hammerjs');
+import { Point } from 'pixi.js'
+import Hammer from 'hammerjs'
 
-/**
- * @description
- * a instance helper to connect hammer to pixijs instances
- * @contructor
- */
-var Connector = function(appView, interactionManager, preInitedHammer) {
-	var self = this;
-	var canvas = appView;
-	
-	this.config = {
-		useOnlyFirstHitTest: true //only hittest on the first event then keep basing all further events off it
-	}
-	
-	var handlers = {};
-	
-	self.interactionManager = interactionManager;
-
-	self.updateCache(canvas);
-	self._options = {};
-
-	// hammer manager
-	if(preInitedHammer){ self._mc = preInitedHammer; }
-	else{ self._mc = new Hammer.Manager(canvas); }
-	// console.log('_mc', self._mc);
-	
-	self.getPixiTarget = function(center){
-		var newCenter = self.normalizePoint(center);
-		return interactionManager.hitTest(newCenter);
-	}
-
-};
-
-/**
- * Just prefix everything hammer-y with hammer- to avoid conflicts
- * @param  {[string]} eventName
- * @return {[string]}
- */
-function decorateEvent(eventName){
-	return "hammer-"+eventName;
+function debounce(func, wait, maxWait) {
+  let lastThis = null
+  let lastArgs = null
+  let lastCallTime
+  let startTime
+  let timeId
+  function startTimer() {
+    return setTimeout(function () {
+      func.apply(lastThis, lastArgs)
+      lastThis = null
+      lastArgs = null
+      lastCallTime = undefined
+      startTime = undefined
+      timeId = undefined
+    }, wait)
+  }
+  return function (...args) {
+    lastThis = this
+    lastArgs = args
+    let currentTime = Date.now()
+    if (timeId) {
+      if (currentTime - startTime < maxWait && currentTime - lastCallTime < wait) {
+        lastCallTime = currentTime
+        clearTimeout(timeId)
+        timeId = startTimer()
+      }
+    } else {
+      lastCallTime = currentTime
+      startTime = currentTime
+      timeId = startTimer()
+    }
+  }
 }
+export default class PixiHammer extends Hammer.Manager {
+  interactionManager = null
+  firstTarget = null
+  registeredEventTypes = []
+  basicEventSuffixs = {
+    'tap': [''],
+    'pan': ['', 'start', 'move', 'end', 'cancel', 'left', 'right', 'up', 'down'],
+    'pinch': ['', 'start', 'move', 'end', 'cancel', 'in', 'out'],
+    'press': ['', 'up'],
+    'rotate': ['', 'start', 'move', 'end', 'cancel'],
+    'swipe': ['', 'left', 'right', 'up', 'down']
+  }
+  _bindListeners = null
+  _unbindListeners = null
 
-/**
- * @description
- * get hammer manager
- */
-Connector.prototype.getManager = function() {
-	var self = this;
-	return self._mc;
-};
+  constructor(canvas, interactionManager, options = {}) {
+    super(canvas, Object.assign({}, Hammer.defaults, options, { recognizers: [] }))
+    this.interactionManager = interactionManager;
 
-/**
- * @description
- * 
- */
-Connector.prototype.registerHandlerTypes = function(typesArray) {
-	var self = this;
-	
-	var first;
-	var firstTarget;
-	
-	self._mc.on("hammer.input", function(evt){
-		if(evt.isFirst){
-			first = evt;
-			firstTarget = self.getPixiTarget(evt.center);
-		}
-		//not clearing these "might" be a problem
-		else if(evt.isFinal){
-			// first = null;
-			// firstTarget = null;
-		}
-	});
-	
-	typesArray.forEach(function(type){
-		self._mc.on(type, function(evt){
-			if(self.config.useOnlyFirstHitTest){ var pixiTarget = firstTarget; }
-			else{ var pixiTarget = self.getPixiTarget(evt.center); }
-			if(pixiTarget){
-				self.interactionManager.dispatchEvent(pixiTarget, decorateEvent(type), evt);
-			}
-		});
-	});
-};
+    // 内部防抖
+    this._bindListeners = debounce(this.bindListeners, 500, 5000)
+    this._unbindListeners = debounce(this.unbindListeners, 500, 5000)
 
-/**
- * @description
- * destroy connector instance
- */
-Connector.prototype.destroy = function() {
-	var self = this;
-	
-	for (var key in self._mc.handlers) {
-		self._mc.off(key, self._mc.handlers[key]);
-	}
-	
-	self._mc.destroy();
-	self._mc = null;
-	self._listeners = {};
-};
+    const recognizers = options.recognizers || Hammer.defaults.preset
+    recognizers.forEach(item => {
+      let recognizer = this.add(new (item[0])(item[1]));
+      item[2] && recognizer.recognizeWith(item[2]);
+      item[3] && recognizer.requireFailure(item[3]);
+    });
+    this.on('hammer.input', (ev) => {
+      // TODO:对于多点触控事件，isFirst是指哪一点，press转pinch会重复执行这一步？
+      if (ev.isFirst) {
+        this.firstTarget = this.getPixiTarget(ev.center);
+      }
+      // 对于tap事件，它在isFinal=true时触发，不能清空，
+      // if (ev.isFinal) {
+      //   firstTarget = null;
+      // }
+    });
+  }
+  decorateEvent(eventName) {
+    return `hammer-${eventName}`
+  }
+  normalizePoint(dstPoint) {
+    let pt = new Point();
+    this.interactionManager.mapPositionToPoint(pt, dstPoint.x, dstPoint.y);
+    return pt;
+  }
+  connect = (ev) => {
+    let pixiTarget = this.firstTarget;
+    let copyEv = Object.assign({}, ev, {
+      stopped: false,
+      stopPropagation() {
+        this.stopped = true
+      }
+    })
+    copyEv.stopPropagation = copyEv.stopPropagation.bind(copyEv)
+    // bubble phase冒泡
+    // pixi内部调用事件listener是同步的，可通过循环控制事件传递
+    while (pixiTarget && !copyEv.stopped) {
+      // 跳过未开启交互的PIXI对象
+      pixiTarget.interactive && this.interactionManager.dispatchEvent(pixiTarget, this.decorateEvent(copyEv.type), copyEv);
+      pixiTarget = pixiTarget.parent;
+    }
+  }
+  bindListeners() {
+    const currentEventTypes = this.getAllEventTypes()
+    const filteredTypes = currentEventTypes.filter(currentType => {
+      return !this.registeredEventTypes.includes(currentType)
+    })
 
-Connector.prototype.normalizePoint = function(dstPoint) {
-	var pt = new PIXI.Point();
-	this.interactionManager.mapPositionToPoint(pt, dstPoint.x, dstPoint.y);
-	return pt;
+    filteredTypes.forEach(type => {
+      this.on(type, this.connect)
+    })
+    this.registeredEventTypes.push(...filteredTypes)
+  }
+  unbindListeners() {
+    const currentEventTypes = this.getAllEventTypes()
+
+    const retainedTypes = []
+    const removedTypes = []
+
+    this.registeredEventTypes.forEach(type => {
+      if (currentEventTypes.includes(type)) {
+        retainedTypes.push(type)
+      } else {
+        removedTypes.push(type)
+      }
+    })
+    // unbind from hammer.manager
+    removedTypes.forEach(type => {
+      this.off(type, this.connect)
+    })
+    this.registeredEventTypes = retainedTypes
+  }
+  add(...args) {
+    const result = super.add(...args)
+    this._bindListeners()
+    return result
+  }
+  getAllEventTypes() {
+    return this.recognizers.reduce((result, recognizer) => {
+      const protoEvent = recognizer.defaults.event
+      const customEvent = recognizer.options.event
+      const suffixs = this.basicEventSuffixs[protoEvent]
+      for (let suffix of suffixs) {
+        result.push(customEvent + suffix)
+      }
+      return result
+    }, [])
+  }
+  remove(...args) {
+    const result = super.remove(...args)
+    this._unbindListeners()
+    return result
+  }
+  getPixiTarget(center) {
+    const newCenter = this.normalizePoint(center);
+    return this.interactionManager.hitTest(newCenter);
+  }
+  destroy() {
+    super.destroy();
+    this._bindListeners = null
+    this._unbindListeners = null
+    this.interactionManager = null
+    this.firstTarget = null
+    this.registeredEventTypes = []
+  }
 }
-
-/**
- * @description
- * recache the canvas bound. call this when the canvas size changed
- * @param {Element?} canvas 
- */
-Connector.prototype.updateCache = function(canvas) {
-	// offset information of this canvas
-	var bound = (canvas || this._mc.element).getBoundingClientRect();
-	this._offset = {
-		x: bound.left,
-		y: bound.top
-	};
-}
-
-module.exports = Connector;
